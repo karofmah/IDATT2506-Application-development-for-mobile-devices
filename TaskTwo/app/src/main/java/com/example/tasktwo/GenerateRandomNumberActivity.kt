9bfefdc6d10075c2a77cd2aadb39472f6b3e44e1
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-09-17 18:45:32 +0200

    Intent may be sent and number may be generated in new activity

--- TaskTwo/app/src/main/java/com/example/tasktwo/GenerateRandomNumberActivity.kt
+++ TaskTwo/app/src/main/java/com/example/tasktwo/GenerateRandomNumberActivity.kt
@@ -1,31 +1,26 @@
 package com.example.tasktwo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class GenerateRandomNumberActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_random_number)
 
-        //val upperLimit = intent.getIntExtra("upperLimit", 100)
-        //println(upperLimit)
-
     }
-/*
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode != RESULT_OK) {
-            Log.e("onActivityResult()", "Something went wrong")
-            return
-        }
 
-        val upperLimit = data?.getIntExtra("upperLimit",100)
-        val value = (0..upperLimit!!).random()
+    fun onClickGenerateRandomNumber(view: View) {
 
-    }*/
+        val upperLimit = intent.getIntExtra("upperLimit", 100)
+        val result = (0..upperLimit).random()
+        Log.i("Result", result.toString())
+        setResult(RESULT_OK, Intent().putExtra("Result", result))
+        finish()
+    }
+
 }
--- TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.kt
+++ TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.kt
@@ -2,29 +2,43 @@ package com.example.tasktwo
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
-private var upperBound = 100;
+private var upperLimit = 100;
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         //displayToast()
     }
-    fun onClickStartGenerateRandomNumber(v: View?) {
+    fun onClickStartGenerateRandomNumberActivity(v: View?) {
         val intent = Intent(this, GenerateRandomNumberActivity::class.java)
-        intent.putExtra("upperBound", upperBound)
-        startActivityForResult(intent,1)
+        intent.putExtra("upperLimit", upperLimit)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            e.message?.let { Log.e("onClickStartGenerateRandomNumberActivity()", it) }
+        }
+    }
+
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (resultCode != RESULT_OK) {
+            Log.e("onActivityResult()", "Something went wrong")
+            return
+        }
+        val upperLimit = data?.getIntExtra("upperLimit",100)
+        Log.i("Upper limit", upperLimit.toString())
+
     }
 
     private fun displayToast(){
