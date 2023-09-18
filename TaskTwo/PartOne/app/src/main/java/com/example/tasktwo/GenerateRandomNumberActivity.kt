650dfe7c4faec8b32ab1b7367a12f3e7648b72ca
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-09-18 14:11:01 +0200

    random number is now generated when new activity is started

--- TaskTwo/PartOne/app/src/main/java/com/example/tasktwo/GenerateRandomNumberActivity.kt
+++ TaskTwo/PartOne/app/src/main/java/com/example/tasktwo/GenerateRandomNumberActivity.kt
@@ -11,9 +11,6 @@ class GenerateRandomNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_random_number)
-    }
-
-    fun onClickGenerateRandomNumber(view: View) {
         val upperLimit = intent.getIntExtra("upperLimit", 100)
         val result = (0..upperLimit).random()
         setResult(RESULT_OK, Intent().putExtra("Result", result))
