98740ddcfad95fac583febf4342af53833bf7cae
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-09-18 14:00:17 +0200

    User may check answer for adding and multiplying

--- TaskTwo/PartTwo/app/src/main/java/com/example/parttwo/MainActivity.kt
+++ TaskTwo/PartTwo/app/src/main/java/com/example/parttwo/MainActivity.kt
@@ -1,37 +1,46 @@
 package com.example.parttwo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun onClickAddButton(v: View){
-
+    private fun  correctAnswer(isAdd: Boolean): Int{
         val numberOneView = findViewById<View>(R.id.numberOneValue) as TextView
         val numberOne: Int = numberOneView.text.toString().toInt()
 
         val numberTwoView = findViewById<View>(R.id.numberTwoValue) as TextView
         val numberTwo: Int = numberTwoView.text.toString().toInt()
 
-        val answerView = findViewById<EditText>(R.id.editAnswer);
+        return if(isAdd)  numberOne + numberTwo else numberOne * numberTwo
+    }
+    fun onClickAddButton(v: View){
+
+        val answerView = findViewById<EditText>(R.id.editAnswer)
         val answer: Int = answerView.text.toString().toInt()
 
-        val correctAnswer = numberOne + numberTwo
-
-        if(correctAnswer == answer){
+        if(correctAnswer(true) == answer){
             Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
         }else{
-            Toast.makeText(this, getString(R.string.incorrect) + " " + correctAnswer, Toast.LENGTH_LONG).show()
+            Toast.makeText(this, getString(R.string.incorrect) + " " + correctAnswer(true), Toast.LENGTH_LONG).show()
         }
     }
-    
+    fun onClickMultiplyButton(v: View){
+
+        val answerView = findViewById<EditText>(R.id.editAnswer)
+        val answer: Int = answerView.text.toString().toInt()
+
+        if(correctAnswer(false) == answer){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this, getString(R.string.incorrect) + " " + correctAnswer(false), Toast.LENGTH_LONG).show()
+        }
+    }
 }
