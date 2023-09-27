15fc52a3333575ae34e7518c95b951158bbd304b
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-09-27 16:26:54 +0200

    Created main layout

---
+++ TaskThree/app/src/main/java/com/example/taskthree/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.taskthree
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.ListView
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    private var dyrenavn: Array<String> = arrayOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun initList() {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, dyrenavn)
+        val listView = findViewById<ListView>(R.id.listView)
+        listView.adapter = adapter
+        listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+        listView.onItemClickListener =
+            OnItemClickListener { parent: AdapterView<*>?, valgt: View, posisjon: Int, id: Long ->
+                findViewById<TextView>(R.id.beskrivelse).text = dyrebeskrivelse[posisjon]
+                findViewById<Spinner>(R.id.spinner).setSelection(posisjon)
+            }
+    }
+}
