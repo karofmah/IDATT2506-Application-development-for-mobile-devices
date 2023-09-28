edb9a6967f416a2710ca006576d226c6cb645455
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-09-28 13:20:41 +0200

    Code clean up

--- TaskThree/app/src/main/java/com/example/taskthree/FriendListAdapter.kt
+++ TaskThree/app/src/main/java/com/example/taskthree/FriendListAdapter.kt
@@ -20,20 +20,10 @@ class FriendListAdapter(private val context: Activity,resource: Int, private val
         val birthDateText: TextView = rowView.findViewById(R.id.birthDate)
 
         nameText.text = friends[position].name
-        Log.d("adapter",friends[position].name)
-        Log.d("adapter",friends[position].birthDate)
-
         birthDateText.text = friends[position].birthDate
 
         return rowView
-        /*
-        val name: String = getItem(position)?.name ?: ""
-        val date: String = getItem(position)?.birthDate ?: ""
-
-        val friend: Friend = Friend(name,date)
-
-        val inflater: LayoutInflater = LayoutInflater.from(context)
-        convertView = inflater.inflate(R.layout.adapter_view,parent,false)*/
+       
     }
 }
 
