e09afd4ad23a3b77856c26ec175c74282bb8d796
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-10-13 19:49:28 +0200

    User may guess number three times after registration

--- TaskFive/app/src/main/java/com/example/taskfive/GameActivity.kt
+++ TaskFive/app/src/main/java/com/example/taskfive/GameActivity.kt
@@ -1,48 +1,67 @@
 package com.example.taskfive
 
-
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
+import okio.ByteString.Companion.encode
 import java.io.IOException
+
 
 class GameActivity : Activity() {
 
+    private val URL = "https://bigdata.idi.ntnu.no/mobil/tallspill.jsp"
 
-    //private val network: HttpWrapper = HttpWrapper(URL)
+    private var guessCount = 0
 
+    private val client = OkHttpClient.Builder()
+        .cookieJar(object : CookieJar {
+            private val myCookieStore = mutableMapOf<HttpUrl, List<Cookie>>()
+
+            override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+                myCookieStore[url] = cookies
+            }
+
+            override fun loadForRequest(url: HttpUrl): List<Cookie> {
+                return myCookieStore[url] ?: listOf()
+            }
+        })
+        .build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
 
-        val button: Button = findViewById(R.id.enterGameButton)
-
-        button.setOnClickListener {
-            performRequest()
+        val registerButton: Button = findViewById(R.id.registerButton)
+        registerButton.setOnClickListener {
+            performRegisterRequest()
         }
-
+        val guessNumberButton: Button = findViewById(R.id.guessNumberButton)
+        guessNumberButton.setOnClickListener {
+            performGuessNumberRequest()
+        }
     }
-    private fun performRequest(){
-        var url = "https://bigdata.idi.ntnu.no/mobil/tallspill.jsp"
+    private fun performRegisterRequest(){
+        guessCount = 0
+
         val name = findViewById<EditText>(R.id.editName).text.toString()
         val cardNumber = findViewById<EditText>(R.id.editCardNumber).text.toString()
 
-        url+="?navn=${name}&kortnummer=${cardNumber}"
-
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url(url)
+        val parameters = FormBody.Builder()
+            .add("navn", name)
+            .add("kortnummer",cardNumber)
             .build()
+
+        val request = Request.Builder()
+            .url(URL)
+            .post(parameters)
+            .addHeader("Content-Type", "text/plain; charset=utf-8")
+            .build()
+
+        Log.d("response", request.toString())
 
         client.newCall(request).enqueue(object: Callback{
             override fun onFailure(call: Call, e: IOException) {
@@ -52,9 +71,42 @@ class GameActivity : Activity() {
             override fun onResponse(call: Call, response: Response) {
                 if(response.isSuccessful){
                     val myResponse = response.body?.string()
-
+                    Log.d("response", response.toString())
                     runOnUiThread {
-                        val resultText = findViewById<TextView>(R.id.enterResultText)
+                        val resultText = findViewById<TextView>(R.id.registerResultText)
+                        resultText.text = myResponse
+                    }
+                }
+            }
+
+        })
+    }
+    private fun performGuessNumberRequest(){
+        val number = findViewById<EditText>(R.id.guessNumberText).text.toString()
+
+        if(guessCount < 3) guessCount++
+        val parameters = FormBody.Builder()
+            .add("tall", number)
+            .build()
+
+        val request = Request.Builder()
+            .url(URL)
+            .post(parameters)
+            .addHeader("Content-Type", "text/plain; charset=utf-8")
+            .build()
+        Log.d("response", request.toString())
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.isSuccessful){
+                    val myResponse = response.body?.string().toString()
+                    Log.d("response", response.toString())
+                    runOnUiThread {
+                        val resultText = findViewById<TextView>(R.id.guessResultText)
                         resultText.text = myResponse
                     }
                 }
